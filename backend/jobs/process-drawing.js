c1730ac7b07e4c9e5a1f95653702437343645f4a
Keari Eggers <kungfu.keari@gmail.com>
2017-01-19 15:23:40 -0600

    remove console statements

--- backend/jobs/process-drawing.js
+++ backend/jobs/process-drawing.js
@@ -14,22 +14,16 @@ cloudinary.config({
 
 module.exports = function processDrawing(params, callback) {
   const id = params.id
-  console.log('starting image processing...')
   const drawing = Drawing.findById(id, (err, drawing) => {
     if(err) return console.log('processDrawing error: ' + err)
-    console.log('found the drawing')
     let image = images(drawing.width * 500, drawing.height * 500)
     drawing.canvasData.forEach((data, i) => {
-      console.log('adding section ' + i)
       const decoded = dataUriToBuffer(data.image)
       image.draw(images(decoded).resize(500), 500 * data.pos[0], 500 * data.pos[1])
     })
     image.save(`images/${id}.png`, { quality: 60 }) 
     cloudinary.uploader.upload(`images/${id}.png`, (result) => {
-      console.log('image uploaded to cloudinary', result)
-      fs.unlink(`images/${id}.png`, () => {
-        console.log('image removed from node server')
-      })
+      fs.unlink(`images/${id}.png`, () => {})
       drawing.canvasData = drawing.canvasData.map((data) => {
         return Object.assign({}, data, { image: null })
       })
@@ -37,7 +31,6 @@ module.exports = function processDrawing(params, callback) {
       drawing.url = result.url
       drawing.save((err, drawing) => {
         if(err) return console.log('drawing save error in jobs/process-drawing')
-        console.log('drawing saved')
         callback()
       })
     })
